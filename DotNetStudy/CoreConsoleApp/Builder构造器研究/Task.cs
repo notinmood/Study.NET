26c50b18e5e52a27265d4206614bed40f09c54f5
解大劦 <develope@163.com>
2024-07-02 11:46:24 +0800

    对本地目录进行整理

---
+++ DotNetStudy/CoreConsoleApp/Builder构造器研究/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CoreConsoleApp.BuilderStudy
+{
+
+    // https://www.cnblogs.com/javastack/p/14704723.html
+    public class Task
+    {
+        private int count;
+        private string name;
+        private DateTime createTime;
+
+        public static class TaskBuilder
+        {
+            private static int count;
+            private static string name;
+            private static DateTime createTime;
+
+            //public static TaskBuilder setName(string name)
+            //{
+            //    TaskBuilder.name = name;
+            //    return this;
+            //}
+
+        }
+    }
+}
---
+++ DotNetStudy/CoreConsoleApp/协变和逆变研究/QuickerIn.cs
@@ -0,0 +1,30 @@
+﻿namespace CoreConsoleApp.协变和逆变研究
+{
+    internal class QuickerIn
+    {
+        public interface Base<in T>
+        {
+            void Test(T param);
+        }
+        public class Sub<T> : Base<T>
+        {
+            public void Test(T param)
+            {
+                //do something;
+            }
+        }
+
+        public void index()
+        {
+            Base<string> myBase1 = new Sub<string>();
+            myBase1.Test("qingdao");
+
+            Base<string> myBase2 = new Sub<object>();
+            myBase2.Test("qingdao");
+
+            //Base<object> myBase3 = new Sub<string>();
+            //myBase3.Test(123);
+        }
+
+    }
+}
