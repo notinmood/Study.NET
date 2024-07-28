cce2876d53ff1613c2f03349f557b43361b4896b
解大劦 <develope@163.com>
2024-07-28 11:44:17 +0800

    将单元测试的代码迁移过来

---
+++ DotNetStudy/CoreConsoleApp/OnlyForTest/MyBiz.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace CoreConsoleApp.OnlyForTest
+{
+    public static class MyBiz
+    {
+        /// <summary>
+        /// 获取三角形类型.
+        /// </summary>
+        /// <param name="sideArr">三角形三边长度数组.</param>
+        /// <returns>返回三角形类型名称.</returns>
+        public static string GetTriangle(int[] sideArr)
+        {
+            if (sideArr.Count() < 3)
+            {
+                return "不构成三角形";
+            }
+
+            int a = sideArr[0];
+            int b = sideArr[1];
+            int c = sideArr[2];
+            if (a + b < c || a + c < b && b + c < a)
+            {
+                return "不构成三角形";
+            }
+
+            if (a == b && a == c)
+            {
+                return "等边三角形";
+            }
+
+            if (a == b || a == c || b == c)
+            {
+                return "等腰三角形";
+            }
+
+            return "一般三角形";
+        }
+    }
+}
---
+++ DotNetStudy/MSTest/UnitTestMyBiz.cs
@@ -0,0 +1,34 @@
+using CoreConsoleApp.OnlyForTest;
+
+namespace MSTest
+{
+    [TestClass]
+    public class UnitTestMyBiz
+    {
+        [TestMethod]
+        public void TestGetTriangle()
+        {
+            int[] sideArr = { 5, 5, 5 };
+            Assert.AreEqual("等边三角形", MyBiz.GetTriangle(sideArr));
+
+
+            int[] sideArr2 = { 5, 5 };
+            Assert.AreEqual("不构成三角形", MyBiz.GetTriangle(sideArr2));
+        }
+
+
+
+        [TestMethod]
+        public void TestMyFoo()
+        {
+            //actual express
+            var actual = true;
+
+            //expected express
+            var expected = true;
+
+            // assert  
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
